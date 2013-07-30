03664103b828006e0c989bb79a7d23e991d14881
John Bellone <john.bellone.jr@gmail.com>
2013-07-30 16:19:34 -0400

    Fix issue with #set_primary_key.
    This should be #primary_key methinks.

--- lib/chaindrive/database.rb
+++ lib/chaindrive/database.rb
@@ -4,12 +4,12 @@ require 'sinatra/sequel'
 # function. In this particular case it is the `users` and `gears` tables.
 migration "initial migration" do
   database.create_table! :users do
-    set_primary_key :id
+    primary_key :id
     column :name, String, :index => true, :null => false
   end
   
   database.create_table! :gears do
-    set_primary_key [:name, :version], :name => 'PkGears'
+    primary_key [:name, :version], :name => 'PkGears'
     column :name, String, :index => true, :null => false
     column :version, String, :index => true, :null => false
     column :repository, String, :null => false
