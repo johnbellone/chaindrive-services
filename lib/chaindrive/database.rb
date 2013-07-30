c4daa1dc5f6ce0472179a843e1ca4424fb5db82d
John Bellone <john.bellone.jr@gmail.com>
2013-07-30 16:05:46 -0400

    Modify the database to use sinatra/sequel.

--- lib/chaindrive/database.rb
+++ lib/chaindrive/database.rb
@@ -1,21 +1,19 @@
-require 'sequel'
+require 'sinatra/sequel'
 
-Sequel::Model.plugin(:schema)
-
-# Define the database table which represents the users that have registered
-# through Omniauth and wish to add a Gear to the registry.
-DB.create_table? :users do
-  set_primary_key :id
-  column :name, String, :index => true, :null => false
-end
-
-# Define the database table which represents the Gears that have been registered
-# through GitHub (or by other means) callbacks.
-DB.create_table? :gears do
-  set_primary_key [:name, :version], :name => 'PkGears'
-  column :name, String, :index => true, :null => false
-  column :version, String, :index => true, :null => false
-  column :repository, String, :null => false
-  column :status, Boolean, :null => false, :default => true
-  foreign_key :user_id, :users, :key => :id
+# Define the database tables that are initially needed for the application to properly
+# function. In this particular case it is the `users` and `gears` tables.
+migration "initial migration" do
+  database.create_table! :users do
+    set_primary_key :id
+    column :name, String, :index => true, :null => false
+  end
+  
+  database.create_table! :gears do
+    set_primary_key [:name, :version], :name => 'PkGears'
+    column :name, String, :index => true, :null => false
+    column :version, String, :index => true, :null => false
+    column :repository, String, :null => false
+    column :status, Boolean, :null => false, :default => true
+    foreign_key :user_id, :users, :key => :id
+  end
 end
