4542910ba6117e9bb06de0c5ba77a02ddd5d2af1
John Bellone <john.bellone.jr@gmail.com>
2013-07-31 11:10:48 -0400

    Refactor and actually require parameters strongly.
    This is a little more complex syntax wise but it strongly requires the
    parameters to be Strings (where proper).

--- lib/chaindrive/api/v1.rb
+++ lib/chaindrive/api/v1.rb
@@ -3,20 +3,34 @@ module Chaindrive
     version 'v1'
     
     namespace :gears do
+      desc "Return all gears inside the registry."
       get do
         compare_etag(Time.now.day)
         Gear.all
       end
 
-      get ':id' do
-        compare_etag(Time.now.day)
-        Gear.where(:name => params[:id], :status => true).order(:created_at).limit(1)
+      params do
+        requires :id, type: String, desc: "Gear name."
+      end
+
+      route_param :id do
+        desc "Return the Gear with specified `id`."
+        get do
+          compare_etag(Time.now.day)
+          Gear.where(:name => params[:id], :status => true).order(:created_at).limit(1)  
+        end
+      end
+
+      params do
+        requires :id, type: String, desc: "Gear name."
+        requires :version, type: String, desc: "Gear version."
       end
 
       get ':id/version/:version' do
         compare_etag(Time.now.day)
         Gear.where(:name => params[:id], :version => params[:version], :status => true)
       end
+
     end
   end
 end
