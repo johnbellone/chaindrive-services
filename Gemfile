9832968d4807969af483dbfaea91158d5034f0fb
John Bellone <john.bellone.jr@gmail.com>
2013-07-31 12:04:14 -0400

    Add support for Grape::Entity and to Gear model.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 ruby '2.0.0'
 
 gem 'grape'
+gem 'grape-entity'
 gem 'omniauth-github'
 gem 'puma'
 gem 'foreman'
--- lib/chaindrive/gear.rb
+++ lib/chaindrive/gear.rb
@@ -3,5 +3,12 @@ require 'sequel'
 module Chaindrive
   class Gear < Sequel::Model
     set_primary_key [:name, :version]
+
+    class Entity < Grape::Entity
+      expose :name
+      expose :version
+      expose :repository
+      expose :created_at
+    end
   end
 end
