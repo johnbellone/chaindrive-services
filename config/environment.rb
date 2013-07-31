ad82c8f1b2f1895b0f56f5735624bfed71a93aa3
John Bellone <john.bellone.jr@gmail.com>
2013-07-31 10:07:00 -0400

    Default some environment variables for development.
    This also makes sense if/when I write tests :D.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
 $:.unshift('lib').uniq!
 
+# Default some values that we would normally expect at runtime.
 ENV['RACK_ENV'] ||= 'test'
+ENV['RACK_SESSION_COOKIE_SECRET'] ||= 'test'
+ENV['DATABASE_URL'] ||= "sqlite://tmp/#{ENV['RACK_ENV']}.db"
 
 require 'rubygems'
 require 'bundler/setup'
