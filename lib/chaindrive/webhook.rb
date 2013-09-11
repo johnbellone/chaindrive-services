4a9463b654a33b42acb89d56048d1619700a31eb
John Bellone <john.bellone.jr@gmail.com>
2013-09-10 21:34:27 -0400

    Fix GitHub webhook to actually accept POST.
    This was an obvious mistake that I wouldn't have caught likely for a
    long while.

--- lib/chaindrive/webhook.rb
+++ lib/chaindrive/webhook.rb
@@ -1,27 +1,31 @@
 # coding: utf-8
 module Chaindrive
   class Webhook < Grape::API
+
     namespace :webhook do
-      desc 'Webhook for GitHub API events'
       namespace :github do
-        event = headers['X-GitHub-Event']
-        delivery_guid = headers['X-GitHub-Delivery']
-        payload = MultiJson.load(params['payload'], symbolize_keys: true)
+        desc 'Webhook for GitHub API events'
+        post do 
+          event = headers['X-GitHub-Event']
+          delivery_guid = headers['X-GitHub-Delivery']
+          payload = MultiJson.load(params['payload'], symbolize_keys: true)
 
-        case event.to_s
-        when 'push'
-          error!('Bad Request', 400) unless payload['repository']
+          case event.to_s
+          when 'push'
+            error!('Bad Request', 400) unless payload['repository']
 
-          # TODO: There is definitely going to be a need for this in the
-          # future. But for now let's just respond back as a failure.
-          error!('Forbidden', 403) if payload['repository']['private']
-          
-          Worker::GearReleaseEvent.perform_async(payload)
-        else
-          error!('Not Implemented', 500)
+            # TODO: There is definitely going to be a need for this in the
+            # future. But for now let's just respond back as a failure.
+            error!('Forbidden', 403) if payload['repository']['private']
+            
+            Worker::GearReleaseEvent.perform_async(payload)
+          else
+            error!('Not Implemented', 500)
+          end
         end
       end
     end
+
   end
 end
 
