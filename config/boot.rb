8f154f6393781ebf7c6f10dbbdc7ce3d522bf2e0
John Bellone <john.bellone.jr@gmail.com>
2013-09-10 21:42:00 -0400

    Fix boot order issue.
    There were some issues with the boot and load order.

---
+++ config/boot.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+require File.join(File.dirname(__FILE__), 'environment')
+require File.join(File.dirname(__FILE__), 'database')
+
+require 'chaindrive'
+
+# Set a default logger that for production environments will write to the
+# system log. From there we can pipe that to logstash.
+if Chaindrive.development?
+  require 'logger'
+  $logger = Logger.new($stdout)
+  $logger.level = Logger::DEBUG
+else
+  require 'syslogger'
+  $logger = Syslogger.new('chaindrive', Syslog::LOG_PID, Syslog::LOG_LOCAL0)
+  $logger.level = Logger::INFO
+end
+
+
--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 $:.unshift('lib').uniq!
 
 # Default some values that we would normally expect at runtime.
-ENV['RACK_ENV'] ||= 'test'
-ENV['RACK_SESSION_COOKIE_SECRET'] ||= 'test'
+ENV['RACK_ENV'] ||= 'development'
+ENV['RACK_SESSION_COOKIE_SECRET'] ||= 'development'
 ENV['DATABASE_URL'] ||= "sqlite://tmp/#{ENV['RACK_ENV']}.db"
 ENV['RACK_CACHE_META_STORE'] ||= 'tmp:/meta'
 ENV['RACK_CACHE_ENTITY_STORE'] ||= 'tmp:/entity'
@@ -12,19 +12,6 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require :default, ENV['RACK_ENV']
 
-require 'chaindrive'
-
-# Set a default logger that for production environments will write to the
-# system log. From there we can pipe that to logstash.
-if Chaindrive.development?
-  require 'logger'
-  $logger = Logger.new($stdout)
-  $logger.level = Logger::DEBUG
-else
-  require 'syslogger'
-  $logger = Syslogger.new('chaindrive', Syslog::LOG_PID, Syslog::LOG_LOCAL0)
-  $logger.level = Logger::INFO
-end
 
 
 
