3400109640f77872a7f0c39dd4e100c3198c1bf8
John Bellone <john.bellone.jr@gmail.com>
2013-10-03 21:10:27 -0400

    Add headers for no robots. Add logger helper.

--- lib/chaindrive/api.rb
+++ lib/chaindrive/api.rb
@@ -6,12 +6,20 @@ module Chaindrive
     default_format :json
     format :json
 
+    before do
+      header 'X-Robots-Tag', 'noindex'
+    end
+
     # Define any helper methods that we want to make available inside of any
     # endpoint.
     helpers do
       def compare_etag(respond_etag)
         error!('Not Modified', 304) if request.env['HTTP_ETAG'] == respond_etag.to_s
         header "ETag", respond_etag.to_s
+      end
+
+      def logger
+        Grape::API.logger
       end
     end
 
